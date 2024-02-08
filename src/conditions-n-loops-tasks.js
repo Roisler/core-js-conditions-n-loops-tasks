1cda8339216024ea50ad47e39660199e93ed1857
Roisler <roislerxs@gmail.com>
2024-02-08 16:52:10 +0700

    feat: implement isContainNumber

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -264,8 +264,14 @@ function getIndexOf(str, letter) {
  *  12345, 0    => false
  *  12345, 6    => false
  */
-function isContainNumber(/* num, digit */) {
-  throw new Error('Not implemented');
+function isContainNumber(num, digit) {
+  const numToString = `${num}`;
+  for (let i = 0; i < numToString.length; i += 1) {
+    if (numToString[i] === `${digit}`) {
+      return true;
+    }
+  }
+  return false;
 }
 
 /**
