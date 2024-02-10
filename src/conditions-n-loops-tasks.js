8809fc4227c94ad5e41bc2e10133906599ad613c
Roisler <roislerxs@gmail.com>
2024-02-10 10:47:38 +0700

    feat: implement canQueenCAptureKing

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -66,8 +66,16 @@ function getMaxNumber(a, b, c) {
  * {x: 1, y: 1}, {x: 2, y: 8} => false
  * {x: 1, y: 1}, {x: 2, y: 8} => false
  */
-function canQueenCaptureKing(/* queen, king */) {
-  throw new Error('Not implemented');
+function canQueenCaptureKing(queen, king) {
+  if (queen.x === king.x || queen.y === king.y) {
+    return true;
+  }
+  const diffX = Math.abs(queen.x - king.x);
+  const diffY = Math.abs(queen.y - king.y);
+  if (diffX === diffY) {
+    return true;
+  }
+  return false;
 }
 
 /**
